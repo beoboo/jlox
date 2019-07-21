073e48c25d62144f8013532a7e211370d50b88c0
Pietrino Atzeni <pietrino.atzeni@gmail.com>
2019-07-21 13:05:08 +0200

    First main

--- src/main/com/craftinginterpreters/jlox/JLox.java
+++
@@ -1,7 +0,0 @@
-package com.craftinginterpreters.jlox;
-
-public class JLox {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
--- src/test/com/craftinginterpreters/jlox/LoxTest.java
+++ src/test/com/craftinginterpreters/jlox/LoxTest.java
@@ -6,7 +6,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class JLoxTest {
+public class LoxTest {
     @Test
     public void TestNothing() {
         assertThat(1, is(equalTo(2)));
